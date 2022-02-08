852677ee36a6c48b2acebed879c66208fbd9edd7
churry97 <63008958+churry97@users.noreply.github.com>
2022-02-08 10:31:03 +0900

    first commit

---
+++ 1008.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double q = sc.nextDouble();
+        double w = sc.nextDouble();
+        System.out.print(q/w);
+    }
+}
